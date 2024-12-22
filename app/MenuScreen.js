9a474ce7142968e40cf2153d456728a48fa7b714
Morillon <julien.morillon@yahoo.fr>
2024-12-22 21:48:20 +0100

    Menu page, recherche d'api

--- app/MenuScreen.js
+++ app/MenuScreen.js
@@ -2,20 +2,58 @@ import {
   Button,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  FlatList,
+  TouchableOpacity
 } from 'react-native';
+import React, { useState } from 'react';
+
+
 
 export default function MenuScreen({ navigation }) {
+
+  const [query, setQuery] = useState('');
+  const [places, setPlaces] = useState([]);
+
+
+
+  const fetchPlaces = async () => {
+    const API_KEY = 'TON_API_KEY_GOOGLE';
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${API_KEY}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPlaces(data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Profile Screen</Text>
-      <Button
-        title="Profile"
-        onPress={() => navigation.navigate('Profile')}
+    <View style={{ padding: 20 }}>
+      <TextInput
+        placeholder="Search for spots"
+        style={{ borderWidth: 1, marginBottom: 10, padding: 8 }}
+        value={query}
+        onChangeText={setQuery}
+      />
+      <Button title="Search" onPress={fetchPlaces} />
+      <FlatList
+        data={places}
+        keyExtractor={(item) => item.place_id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Text style={{ marginVertical: 10 }}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
-}
+};
+
+
 
 const styles = StyleSheet.create({
   container: {
